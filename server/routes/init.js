eaf5e82d6935b9030b0a6b73a43e9eff0b0d2859
Aditya Parmar <parmar415@gmail.com>
2017-10-07 13:42:18 -0700

    develop the initial database operations

---
+++ server/routes/init.js
@@ -0,0 +1,38 @@
+var mysql = require('mysql');
+
+function create_database_tables(){
+    var con = mysql.createConnection({
+      host: "localhost",
+      user: "root",
+      password: "root"
+    });
+    
+    con.connect(function(err) {
+          if (err) throw err;
+          console.log("Connected!");
+          con.query("CREATE DATABASE IF NOT EXISTS dropbox", function (err, result) {
+            if (err) throw err;
+            console.log("Database created");
+            //Table Create
+            //userdata table 
+            var userdata="CREATE TABLE IF NOT EXISTS `dropbox`.`userdata` ( \
+              `userid` INT NOT NULL AUTO_INCREMENT,\
+              `email` VARCHAR(45) NULL,\
+              `password` VARCHAR(45) NULL,\
+              `firstname` VARCHAR(45) NULL,\
+              `lastname` VARCHAR(45) NULL,\
+              PRIMARY KEY (`userid`));"
+  
+             con.query(userdata, function (err, result) {
+              if (err) throw err;
+              console.log("Table userdata created");
+              
+            //File table   
+              
+            });
+          });
+        });
+  }
+module.exports={
+    create_database_tables:create_database_tables 
+}
